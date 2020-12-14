371f7873061b2ce530c9979d31033b4703051502
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-12-14 22:23:57 +0530

    StreamKafkaApplication class with main()

---
+++ stream-kafka/src/main/java/com/zeeshan/sk/StreamKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.zeeshan.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamKafkaApplication.class, args);
+	}
+
+}
